9045a81509cb5d52b98ed1dd0f167581ee226a67
milan <milanasad15@gmail.com>
2024-04-09 13:00:54 +0100

    Search bar implementation

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header'; // Import Header component from the same director
 
 export default function App() {
     const [recipes, setRecipes] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
         fetchRecipes();
@@ -11,7 +12,6 @@ export default function App() {
     const fetchRecipes = async () => {
         try {
             const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=c54ba6b8202141d69cd028ce39a0e087&number=5`);
-
             const data = await response.json();
             setRecipes(data.recipes);
         } catch (error) {
@@ -19,9 +19,35 @@ export default function App() {
         }
     };
 
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const handleSearchSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=c54ba6b8202141d69cd028ce39a0e087&query=${searchQuery}`);
+            const data = await response.json();
+            setRecipes(data.results);
+        } catch (error) {
+            console.error('Error searching for recipes:', error);
+        }
+    };
+
     return (
         <div className="master-wrapper">
             <Header />
+            <div className="search-container">
+                <form onSubmit={handleSearchSubmit}>
+                    <input
+                        type="text"
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                        placeholder="Search for recipes"
+                        className="search-bar"
+                    />
+                </form>
+            </div>
             <div className="recipe-cards-wrapper">
                 {recipes.map(recipe => (
                     <a key={recipe.id} className="recipe-card" href={recipe.sourceUrl} target="_blank" rel="noopener noreferrer">
